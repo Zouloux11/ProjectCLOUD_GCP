eef0bf04db3d36dc02533adc63c5f82814608d38
Loic Capdeville <caploic@outlook.fr>
2026-01-26 20:24:54 +0100

    [Ansible] IAM with service user

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,6 +19,6 @@ resource "google_project_iam_member" "admins" {
   for_each = toset(var.authorized_users)
   
   project = var.gcp_project
-  role    = "roles/compute.osLogin"
+  role    = "roles/compute.osAdminLogin"
   member  = "user:${each.value}"
 }
