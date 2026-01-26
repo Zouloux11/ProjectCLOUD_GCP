28961a7c63a3ed842c28056968575a6e456f762f
antoinetelescope <lescopantoine@gmail.com>
2026-01-26 22:21:41 +0100

    [terraform] clean du code

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,16 +1,14 @@
-resource "google_compute_firewall" "allow_ssh" {
-  name    = "allow-ssh"
-  network = google_compute_network.custom_vpc.name
-
+resource "google_compute_firewall" "allow_ssh_iap" {
+  name          = "allow-ssh-iap"
+  network       = google_compute_network.custom_vpc.name
+  source_ranges = ["35.235.240.0/20"]
+  target_tags   = ["master", "workers", "edge"]
+  
   allow {
     protocol = "tcp"
     ports    = ["22"]
   }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["master", "workers", "edge"]
 }
-
 
 resource "google_compute_firewall" "allow_internal" {
   name    = "allow-internal"
@@ -29,4 +27,30 @@ resource "google_compute_firewall" "allow_internal" {
   }
 
   source_ranges = ["10.0.0.0/24"]
+}
+
+resource "google_compute_firewall" "allow_spark_ui" {
+  name    = "allow-spark-ui"
+  network = google_compute_network.custom_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  description   = "Allow Spark UI access (from outside)"
+}
+
+resource "google_compute_firewall" "allow_hdfs_ui" {
+  name    = "allow-hdfs-ui"
+  network = google_compute_network.custom_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["50070", "9870"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  description   = "Allow HDFS UI access (from outside)"
 }
